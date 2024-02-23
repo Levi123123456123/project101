d8267b6e81be202f527b45d10b04e6e769fdc2b1
Levi123123456123 <158478881+Levi123123456123@users.noreply.github.com>
2024-02-23 14:27:03 +0300

    Fzcdhsft
    fhdxzcfhd

---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    private float Fallvelocity = 1;
+    public float gravity = 9.8F;
+    private CharacterController charcontrl;
+    public float jmfrce;
+    public float smeed;
+    private Vector3 vectr;
+    void Start()
+    {
+        charcontrl = GetComponent<CharacterController>();
+    }
+     void Update()
+    {
+        vectr = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+            {
+                vectr += transform.forward;
+            }
+        if(Input.GetKey(KeyCode.S))
+            {
+                vectr -= transform.forward;
+            }
+        if(Input.GetKey(KeyCode.D))
+            {
+                vectr += transform.right;
+            }
+        if(Input.GetKey(KeyCode.A))
+            {
+                vectr -= transform.right;
+            }
+        if (charcontrl.isGrounded)
+            {
+                Fallvelocity = 0;
+            }
+        if (Input.GetKeyDown(KeyCode.Space) && charcontrl.isGrounded)
+            {
+                Fallvelocity = - jmfrce;
+            }
+    }
+    void FixedUpdate()
+    {
+        charcontrl.Move(vectr*Time.fixedDeltaTime*smeed);
+        Fallvelocity += gravity * Time.fixedDeltaTime;
+        charcontrl.Move(Vector3.down*Fallvelocity*Time.fixedDeltaTime);
+    }
+}
